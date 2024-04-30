2053418290cbf9fcf554de9a0fbf7d5dd32dd77b
Leah B <leahb2613@gmail.com>
2024-04-29 21:56:02 -0500

    look and feel

--- GameDriver.java
+++ GameDriver.java
@@ -5,6 +5,9 @@ public class GameDriver{
     private ConfigFrame config;
     
     public GameDriver() {
+        try { 
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel"); 
+        } catch(Exception ignored){}
 
         ActionListener startListener = new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -33,24 +36,6 @@ public class GameDriver{
     }
 
     public static void main(String[] args) {
-        try {
-            // Set cross-platform Java L&F (also called "Metal")
-        UIManager.setLookAndFeel(
-            UIManager.getCrossPlatformLookAndFeelClassName());
-        } 
-        catch (UnsupportedLookAndFeelException e) {
-        // handle exception
-        }
-        catch (ClassNotFoundException e) {
-        // handle exception
-        }
-        catch (InstantiationException e) {
-        // handle exception
-        }
-        catch (IllegalAccessException e) {
-        // handle exception
-        }
-        
-        GameDriver newGame = new GameDriver();
+        new GameDriver();
     }
 }
