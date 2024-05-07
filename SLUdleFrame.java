80739de8a26145a414b536709f62a64df0121f80
sahanagujja <157167948+sahanagujja@users.noreply.github.com>
2024-05-06 20:21:25 -0500

    Update SLUdleFrame.java

--- SLUdleFrame.java
+++ SLUdleFrame.java
@@ -175,11 +175,11 @@ public class SLUdleFrame extends JFrame {
         if(result){
             messageLabel.setText("You Won :)");
             keyboard.disableKeyboard();
-            //calculator.updateStats(guessCount);
+            calculator.updateStats(1);
         } else if (guessCount == maxGuess){
             messageLabel.setText("Answer: " + secretWord);
             keyboard.disableKeyboard();
-            //calculator.updateStats(guessCount);
+            calculator.updateStats(0);
         
         }
         guessCount++;
